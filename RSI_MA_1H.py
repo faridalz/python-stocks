d04480a443be9b162872b3902a9a188c8eaf3308
Farid Shiraliyev <79901122+faridalz@users.noreply.github.com>
2026-05-17 19:37:45 +0200

    Update RSI_MA_1H.py

--- RSI_MA_1H.py
+++ RSI_MA_1H.py
@@ -214,6 +214,34 @@ def run_scan():
     "FIL-USD",    # Filecoin
     "OP-USD",     # Optimism
     "ARB-USD",    # Arbitrum
+    "B-USD",
+    "SUI20947-USD",
+    "TON11419-USD",
+    "ME32197-USD",
+    "TRUMP35336-USD",
+    "LTC-USD",
+    "SOL16116-USD",
+    "ONDO-USD",
+    "PROS39682-USD",
+    "LAB33223-USD",
+    "KITE-USD",
+    "ASTER36341-USD",
+    "AIA38430-USD",
+    "SPK36569-USD",
+    "UB38339-USD",
+    "OSMO-USD",
+    "ARB11841-USD",
+    "SAGA30372-USD",
+    "PHB-USD",
+    "STORJ-USD",
+    "CGPT-USD",
+    "SAHARA-USD",
+    "VIRTUAL-USD",
+    "POL28321-USD",
+    "BSB38889-USD",
+        
+        
+        
  
     # --- Tier 3: Large-cap DeFi / Layer 1 ---
     "SUI-USD",    # Sui
@@ -256,6 +284,23 @@ def run_scan():
     "ENA-USD",    # Ethena
     "ETHFI-USD",  # Ether.fi
     "REZ-USD",    # Renzo
+    "4-USD",
+    "SKYAI-USD",
+    "FLOCK-USD",
+    "VANRY-USD",
+    "SXT-USD",
+    "CHZ-USD",
+    "PEPE24478-USD",        
+    "MON30495-USD",
+    "ARKM-USD",
+    "SHIB-USD",
+    "XAUT-USD",
+    "LAYER35429-USD",
+    "AI39883-USD",
+        
+        
+        
+        
  
     # --- Tier 7: Gaming / NFT / metaverse ---
     "BEAM-USD",   # Beam
@@ -263,6 +308,16 @@ def run_scan():
     "YGG-USD",    # Yield Guild Games
     "PYR-USD",    # Vulcan Forged
     "LOOKS-USD",  # LooksRare
+    "HYPE32196-USD",    
+    "USDC-USD",
+    "BILL39545-USD",
+    "OPG-USD",   
+    "UNI7083-USD",
+    "XPL-USD",
+    "ROBO39595-USD",
+    "COMP5692-USD",
+    "STABLE38892-USD",
+    "TRIA-USD",
  
     # --- Tier 8: Layer 2 / rollups ---
     "MANTA-USD",  # Manta Network
@@ -283,6 +338,11 @@ def run_scan():
     "AIOZ-USD",   # AIOZ Network
     "RSS3-USD",   # RSS3
     "NMR-USD",    # Numeraire
+    "IRYS-USD",
+    "SUN-USD",
+    "ORCA-USD",
+        
+        
  
     # --- Tier 10: Classic / legacy alts ---
     "XLM-USD",    # Stellar
@@ -294,7 +354,6 @@ def run_scan():
     "XTZ-USD",    # Tezos
     "ICX-USD",    # ICON
     "ONT-USD",    # Ontology    
-
     ]
     
     results = []
